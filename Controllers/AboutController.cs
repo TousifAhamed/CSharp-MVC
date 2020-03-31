320c429b49415b624719512ede6f52141af7b8cc
TousifAhamed <4966926+TousifAhamed@users.noreply.github.com>
2020-03-31 19:20:06 +0300

    Add files via upload
    This project demonstrates basic setup of C# MVC WEB Application

---
+++ Controllers/AboutController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication1.Controllers
+{
+    public class AboutController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            //List<String> Language = new List<string>();
+            //Language.Add("Python");
+            //Language.Add("CSharp");
+            //Language.Add("Java");
+            //Language.Add("C++");
+            //Language.Add("JavaScript");
+            //Language.Add("jQuery");
+            string language = "Hello I am python developer";
+
+            ViewBag.language = language;
+            return View();
+        }
+
+        public ActionResult GetData()
+        {
+            //string result = "This is about Page";
+            //string result = "https://www.google.com/";
+            //return Content(result, "text/plain");
+            //return Redirect(result);
+            // Redirecting to same controller
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
